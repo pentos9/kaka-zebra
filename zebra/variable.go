0ffa9e0e126f173a89650517b6a40da1c5411b1c
lucas ley <ayaaa168@163.com>
2018-12-13 23:33:46 +0800

    feat(Variable):add variable definition test

---
+++ zebra/variable.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func run() {
+	var number int = 12
+	fmt.Println(number)
+
+	number = 1500
+	fmt.Println(number)
+
+	newNumber := 567
+	fmt.Println(newNumber)
+
+}
+
+func main() {
+	run()
+}
