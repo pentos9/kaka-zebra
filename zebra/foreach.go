cc2dcf654b8312cff73e899b785b63050fb8f61b
lucas <lucas@Lucass-MacBook-Pro.local>
2018-12-16 23:05:02 +0800

    test(ForEach):add foreach test

---
+++ zebra/foreach.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func foreach() {
+	sum := 0
+	for i := 0; i < 10; i++ {
+		sum += i
+	}
+
+	fmt.Println(sum)
+
+	for ; sum < 1000;  {
+		sum += sum
+	}
+
+	fmt.Println(sum)
+}
+
+func main() {
+	foreach()
+}
