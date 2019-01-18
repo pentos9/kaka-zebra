8f40db4810aa78eb4562615cecff06f714584c07
lucas ley <ayaaa168@163.com>
2019-01-18 16:17:06 +0800

    test(Slice):add slice range test

--- zebra/slice_len_cap.go
+++ zebra/slice_len_cap.go
@@ -6,6 +6,18 @@ func printSlice(s []int) {
 	fmt.Printf("len=%d,cap=%d,%v \n", len(s), cap(s), s)
 }
 
+func doPower() {
+	var power = []int{1, 3, 4, 5, 5, 6, 6,}
+	fmt.Println(power)
+
+	for i := range power {
+		power[i] = 1 << uint(i) //resign value
+	}
+
+	fmt.Println(power)
+
+}
+
 func main() {
 	s := []int{2, 3, 5, 7, 11, 13}
 	printSlice(s)
@@ -25,4 +37,9 @@ func main() {
 	if array == nil {
 		fmt.Println("nil!")
 	}
+
+	array = append(array, 98)
+	fmt.Println(array)
+
+	doPower()
 }
